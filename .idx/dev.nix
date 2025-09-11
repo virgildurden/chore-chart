d9d3f8258798576e09debaf08904ba762235c7df
virgil.durden <virgil.durden@gmail.com>
2025-09-11 15:01:32 +0000

    fix firebase env and print gui

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -7,14 +7,12 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
-    # pkgs.python311
+    pkgs.python311
     # pkgs.python311Packages.pip
     # pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
   ];
 
-  # Sets environment variables in the workspace
-  env = {};
   idx = {
     # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
@@ -25,16 +23,10 @@
     previews = {
       enable = true;
       previews = {
-        # web = {
-        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
-        #   # and show it in IDX's web preview panel
-        #   command = ["npm" "run" "dev"];
-        #   manager = "web";
-        #   env = {
-        #     # Environment variables to set for your server
-        #     PORT = "$PORT";
-        #   };
-        # };
+        web = {
+          command = [ "sh" "-c" "python3 -m http.server $PORT" ];
+          manager = "web";
+        };
       };
     };
 
